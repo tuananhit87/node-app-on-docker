0ec729bffeeca3f4a66753c077faeee7d2ed50a3
tuananhit87 <ngtuananh87@gmail.com>
2021-01-14 21:27:33 +0700

    .

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import express from "express";
+const express = require("express");
+import cors from "cors";
 import { config } from "dotenv";
 config();
 
@@ -6,8 +7,21 @@ const { HOST, PORT } = process.env;
 
 const app = express();
 
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/hello", (req, res) => {
+  res.send({ message: "Hello World" });
+});
+
+app.post("/user", (req, res) => {
+  res.send({ message: `Do you want to find user '${req.body.username}'?` });
 });
 
 app.listen(PORT, HOST);
